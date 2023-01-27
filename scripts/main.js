45eb9437fef62b2abff608bab44a0423fa437d08
“Eveline <eveline-c@hotmail.com>
2023-01-27 17:56:53 +0000

    test 3

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+/** @type{HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const startButton = document.getElementById('start');
+
+const player = new Component(220, 550, 50, 100, "image", ctx); 
+
+startButton.onclick = function (){
+    
+    const game = new Game(ctx, 1350, 900, player);
+    game.start();
+
+}; 
+
+document.addEventListener("keydown", (e) => {
+switch(e.code){
+    case "ArrowLeft": 
+    player.speedX -= 2;
+    break;
+    case "ArrowRight": 
+    player.speedX += 2;
+    break;
+
+}
+}); 
+
+document.addEventListener("keyup", () => {
+    player.speedX = 0; 
+    player.speedY = 0; 
+});
